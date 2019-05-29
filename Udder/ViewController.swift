6ae12a2a0def7db514ac33bcdd639695ecdb2674
ChrisZcmu <siyuz2@andrew.cmu.edu>
2019-05-29 15:18:18 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Siyu Zhang on 5/29/19.
+//  Copyright © 2019 Siyu Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
